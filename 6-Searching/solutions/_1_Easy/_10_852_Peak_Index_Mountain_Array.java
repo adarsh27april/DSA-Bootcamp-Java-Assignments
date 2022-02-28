009a888cb71f3299aee5b68b5dbb5d78b2cbdfe5
adarsh27april <adarshkumar27april@gmail.com>
2022-02-28 10:44:04 +0530

    LC 852 peak In Mountain Array updated with Kunal's solution

--- 6-Searching/solutions/_1_Easy/_10_852_Peak_Index_Mountain_Array.java
+++ 6-Searching/solutions/_1_Easy/_10_852_Peak_Index_Mountain_Array.java
@@ -4,6 +4,7 @@ public class _10_852_Peak_Index_Mountain_Array {
     public static void main(String[] args) {
         int arr[] = { 3, 5, 3, 2, 0 };
         System.out.println(Solution10.peakIndexInMountainArray(arr));
+        System.out.println(Solution10.peakIndexInMountainArray_KunalSoln(arr));
     }
 }
 
@@ -26,5 +27,27 @@ class Solution10 {
             }
         }
         return -1;
+    }
+
+    public static int peakIndexInMountainArray_KunalSoln(int[] arr) {
+        /**
+         * ex arr : [1, 2, 3, 5, 6, 4, 3, 2]
+         */
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                // descending part of arr
+                end = mid;
+
+            } else {
+                // ascending part of arr
+                // arr[mid] < arr[mid+1]
+                start = mid + 1;
+            }
+        }
+        return start;
+        // at the end start == end == peakElement
     }
 }
