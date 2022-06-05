00b705045c4baf35163575c759a6e261f609d12e
adarsh27april <adarshkumar27april@gmail.com>
2022-06-05 21:34:48 +0530

    solution -> code

--- 6-Searching/code/_1_Easy/_9_35_Search_Insert_Position.java
+++ 6-Searching/code/_1_Easy/_9_35_Search_Insert_Position.java
@@ -2,34 +2,23 @@ package _1_Easy;
 
 public class _9_35_Search_Insert_Position {
     public static void main(String[] args) {
-        int nums[] = { 1, 3, 5, 6 },
-                // target = 2;
-                target = 7;
-        // target = 5;
+        int nums[] = { 1, 3, 5, 6 };
+        int target = 7;
         System.out.println(Solution9.searchInsert(nums, target));
     }
 }
 
 class Solution9 {
     public static int searchInsert(int[] nums, int target) {
-
-        int start = 0, n = nums.length, end = n;
-
-        if (nums[n - 1] < target) {
-            // corner case
-            return n;
-        }
-
+        int start = 0, end = nums.length - 1;
         int mid;
         while (start <= end) {
             mid = start + (end - start) / 2;
-
             if (nums[mid] < target) {
                 start = mid + 1;
 
             } else if (nums[mid] > target) {
                 end = mid - 1;
-
             } else {
                 return mid;
             }
---
+++ 6-Searching/code/_2_Medium/_4_81_Search_Rotated_Sorted_Array_II.java
@@ -0,0 +1,7 @@
+package _2_Medium;
+
+public class _4_81_Search_Rotated_Sorted_Array_II {
+   public static void main(String[] args) {
+
+   }
+}
