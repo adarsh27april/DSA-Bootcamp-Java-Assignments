2ef0e6d94bc1e193d52ffc7e77473750550be59a
adarsh27april <adarshkumar27april@gmail.com>
2022-06-05 22:42:35 +0530

    LC Medium 81

--- 6-Searching/code/Video_Lecture/Pivot_Rotated_Sorted_Array_with_Duplicates.java
+++ 6-Searching/code/Video_Lecture/Pivot_Rotated_Sorted_Array_with_Duplicates.java
@@ -1,39 +1,13 @@
-package _2_Medium;
+package Video_Lecture;
 
-public class _4_81_Search_Rotated_Sorted_Array_II {
+public class Pivot_Rotated_Sorted_Array_with_Duplicates {
    public static void main(String[] args) {
-      Solution4 ob = new Solution4();
-      int nums[] = { 3, 5, 1 }, target = 3;
-      System.out.println(ob.search(nums, target));
-   }
-}
-
-class Solution4 {
-   public boolean search(int[] nums, int target) {
-
-      if (nums.length == 1) {
-         if (target == nums[0]) {
-            return true;
-         }
-         return false;
-      }
-
-      int pivot = findPivot(nums);
-      int ans = -1;
-
-      if (pivot > -1) {
-         if (binarySearch(nums, target, 0, pivot)) {
-            return true;
-         }
-
-         return binarySearch(nums, target, pivot + 1, nums.length - 1);
-      } else {
-         return binarySearch(nums, target, 0, nums.length - 1);
-      }
-
+      // int arr[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1 };
+      int arr[] = { 1, 1, 3 };
+      System.out.println(findPivot(arr));
    }
 
-   public int findPivot(int[] arr) {
+   static int findPivot(int[] arr) {
       int start = 0, end = arr.length - 1;
       while (start <= end) {
          int mid = start + (end - start) / 2;
@@ -74,22 +48,5 @@ class Solution4 {
       }
 
       return -1;// i.e., array doesn't have pivot
-   }
-
-   public boolean binarySearch(int[] arr, int target, int start, int end) {
-      while (start <= end) {
-         int mid = start + (end - start) / 2;
-
-         if (arr[mid] == target) {
-            return true;
-         }
-
-         if (arr[mid] < target) {
-            start = mid + 1;
-         } else {
-            end = mid - 1;
-         }
-      }
-      return false;
    }
 }
