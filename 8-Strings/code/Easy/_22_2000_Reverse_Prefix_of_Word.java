27c6ecdb54a3b15e033bd14c6fb7c380036a5164
adarsh27april <adarshkumar27april@gmail.com>
2022-09-04 09:47:43 +0530

    LC easy 2000

---
+++ 8-Strings/code/Easy/_22_2000_Reverse_Prefix_of_Word.java
@@ -0,0 +1,27 @@
+package Easy;
+
+public class _22_2000_Reverse_Prefix_of_Word {
+   public static void main(String[] args) {
+      Solution22 ob = new Solution22();
+      String s = ob.reversePrefix("abcdefd", "d");
+      System.out.println(s);
+   }
+}
+
+
+class Solution22 {
+   public String reversePrefix(String word, char ch) {
+      int x = word.indexOf(ch);
+      StringBuilder s = new StringBuilder();
+      for (int i = x; i >= 0; i--) {
+         s.append(word.charAt(i));
+      }
+      if (x == word.length()) {
+         return s.toString();
+      }
+      for (int i = x + 1; i < word.length(); i++) {
+         s.append(word.charAt(i));
+      }
+      return s.toString();
+   }
+}
