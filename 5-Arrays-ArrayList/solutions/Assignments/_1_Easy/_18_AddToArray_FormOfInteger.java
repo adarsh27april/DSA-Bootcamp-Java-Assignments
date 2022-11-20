2bae3eb836c3846aff722f29125f4b1e21e744cc
adarsh27april <adarshkumar27april@gmail.com>
2022-11-21 02:24:11 +0530

    LC easy 989 update

--- 5-Arrays-ArrayList/solutions/Assignments/_1_Easy/_18_AddToArray_FormOfInteger.java
+++ 5-Arrays-ArrayList/solutions/Assignments/_1_Easy/_18_AddToArray_FormOfInteger.java
@@ -6,15 +6,20 @@ import java.util.List;
 
 public class _18_AddToArray_FormOfInteger {
     public static void main(String[] args) {
-        int[] num = { 9, 9, 9 };
+        int[] num = {9, 9, 9};
         Solution18 ob = new Solution18();
         ob.addToArrayForm(num, 1);
     }
 }
 
+
 class Solution18 {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> numList = new ArrayList<>(Collections.nCopies(num.length, 0));
+        // List<Integer> numList = new ArrayList<>(Collections.nCopies(num.length, 0));
+        List<Integer> numList = new ArrayList<>();
+        for (int i : num)
+            numList.add(i);
+
         int carry = k;
         for (int i = num.length - 1; i >= 0; i--) {
             numList.set(i, carry + num[i]);
